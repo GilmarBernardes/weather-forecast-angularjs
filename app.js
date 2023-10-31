37b5eba501011a9ca96e4b48c8eb75ac12d20532
Gilmar Bernardes <galtran.void@gmail.com>
2023-10-31 22:23:43 +0000

    creating form to get forecast by city. adjusted all files

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 // Module
-var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResources']);
+var weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource']);
 
 // Routes
 weatherApp.config(function ($routeProvider) {
+   
     $routeProvider
+    
     .when('/', {
-        templateUrl: 'pages/home.html',
+        templateUrl: 'pages/home.htm',
         controller: 'homeController'
     })
+    
     .when('/forecast', {
-        templateUrl: 'pages/forecast.html',
+        templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
-    });
+    })
+    
 });
 
 // Controllers
 weatherApp.controller('homeController', ['$scope', function($scope) {
-
+    
 }]);
 
 weatherApp.controller('forecastController', ['$scope', function($scope) {
-
+    
 }]);
